8ba051437a92c1b82a07972881c1dc19b5f22f7a
anpeep <anpeep@taltech.ee>
2025-11-30 11:07:00 +0200

    added dependencies and Google Maps API

---
+++ GPSSportMap/build.gradle.kts
@@ -0,0 +1,11 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("androidx.room") version "2.8.2"
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false // ✅ NEW
+
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.28" apply false
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+}
